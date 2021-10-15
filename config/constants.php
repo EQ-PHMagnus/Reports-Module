5d7a77abad54cb6016c1781e34243b0f72fc3b14
lilay05 <fporazo@phmagnus.com>
2021-10-15 16:45:45 -0700

    Fixed raven urls constants

--- config/constants.php
+++ config/constants.php
@@ -9,27 +9,27 @@ return [
                 'title'                 =>  'Total Bets Count ',
                 'content_title'         =>  'Total Count of Bets',
                 'type'                  =>  'count',
-                'url'                 =>  url('raven/bets/total-count-bets')
+                'url'                   =>  config('app.url').('raven/bets/total-count-bets')
             ],
             'total-amount-bets'    =>  [
                 'title'                 =>  'Total Bets Amount ',
                 'content_title'         =>  'Total Amount of Bets',
                 'type'                  =>  'sum',
-                'url'                 =>  url('raven/bets/total-amount-bets')
+                'url'                   =>  config('app.url').('raven/bets/total-amount-bets')
             ],
             'total-count-bets-arena'    =>  [
                 'title'                 =>  'Total Bets Count per Arena ',
                 'content_title'         =>  'Total Count of Bets per Arena',
                 'type'                  =>  'sum',
                 'name'                  =>  'arena',
-                'url'                 =>  url('raven/bets/total-count-bets-arena')
+                'url'                   =>  config('app.url').('raven/bets/total-count-bets-arena')
             ],
             'total-amount-bets-arena'    =>  [
                 'title'                 =>  'Total Bets Amount per Arena ',
                 'content_title'         =>  'Total Amount of Bets per Arena',
                 'type'                  =>  'sum',
                 'name'                  =>  'arena',
-                'url'                   =>  url('raven/bets/total-amount-bets-arena')
+                'url'                       =>  config('app.url').('raven/bets/total-amount-bets-arena')
             ],
         ],
 
@@ -38,14 +38,14 @@ return [
                     'title'                 =>  'Total Fights Count ',
                     'content_title'         =>  'Total Count of Fights',
                     'type'                  =>  'count',
-                    'url'                   =>  url('raven/fights/total-count-fights')
+                    'url'                       =>  config('app.url').('raven/fights/total-count-fights')
                 ],
                 'total-count-fights-arena'    =>  [
                     'title'                 =>  'Total Fights Count per Arena ',
                     'content_title'         =>  'Total Count of Fights per Arena',
                     'type'                  =>  'count',
                     'name'                  =>  'arena',
-                    'url'                   =>  url('raven/fights/total-count-fights-arena')
+                    'url'                       =>  config('app.url').('raven/fights/total-count-fights-arena')
                 ],
                
         ],
@@ -54,13 +54,13 @@ return [
                 'title'                 =>  'Agent Deposits Pending',
                 'content_title'         =>  'Pending List',
                 'type'                  =>  'pending',
-                'url'                   =>  url('raven/agent-deposits/pending')
+                'url'                       =>  config('app.url').('raven/agent-deposits/pending')
             ],
             'processed'    =>  [
                 'title'                 =>  'Agent Deposits Processed',
                 'content_title'         =>  'Processed List',
                 'type'                  =>  'processed',
-                'url'                   =>  url('raven/agent-deposits/processed')
+                'url'                       =>  config('app.url').('raven/agent-deposits/processed')
             ],
            
     ]
