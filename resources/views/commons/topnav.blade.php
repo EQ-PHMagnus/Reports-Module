36c2763bcef68a1c90204aa0d502d3f6865224c9
zik <ezekielquintans@gmail.com>
2021-11-10 11:39:38 +0000

    ###Removed page transitions

--- resources/views/commons/topnav.blade.php
+++ resources/views/commons/topnav.blade.php
@@ -23,14 +23,14 @@
             <div class="collapse navbar-collapse navbar-collapse-toolbar" id="site-navbar-collapse">
                 <!-- Navbar Toolbar -->
                 <ul class="nav navbar-toolbar">
-                    <li class="nav-item hidden-float" id="toggleMenubar">
+                    <!-- <li class="nav-item hidden-float" id="toggleMenubar">
                         <a class="nav-link" data-toggle="menubar" href="#" role="button">
                             <i class="icon hamburger hamburger-arrow-left">
                                 <span class="sr-only">Toggle menubar</span>
                                 <span class="hamburger-bar"></span>
                             </i>
                         </a>
-                    </li>
+                    </li> -->
                     <li class="nav-item hidden-sm-down" id="toggleFullscreen">
                         <a class="nav-link icon icon-fullscreen" data-toggle="fullscreen" href="#" role="button">
                             <span class="sr-only">Toggle fullscreen</span>
