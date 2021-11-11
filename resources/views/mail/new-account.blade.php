e3cb84db46829fc4861084544a919998771aad99
lilay05 <fporazo@phmagnus.com>
2021-11-11 22:59:18 +0800

    Added link to login in email notification newly added account

--- resources/views/mail/new-account.blade.php
+++ resources/views/mail/new-account.blade.php
@@ -8,6 +8,8 @@
     <h4>This email includes your account details, so please keep it safe.</h4>
     <p><b>Your email: {{$details['user']['email']}}</b></p>
     <p><b>Your password: {{$details['user']['password']}}</b></p>
+
+    <p><b>Click <a href="{{url('/login')}}">here</a> to login</b></p>
    
     <p>Thank you</p>
 </body>
